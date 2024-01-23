cfdd3682411d65d10dab0dda0845a3be7c80b735
deemh <de.alboqmi@gmail.com>
2024-01-23 10:18:52 +0300

    add

---
+++ routineTick/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  routineTick
+//
+//  Created by Deemh Albaqami on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
