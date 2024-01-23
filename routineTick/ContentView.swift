b0c8525a367f87f0ce967db5f70c890633e4bd50
noraaldossary <nnxh.1@hotmail.com>
2024-01-23 09:16:50 +0300

    Initial Commit

---
+++ routineTick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  routineTick
+//
+//  Created by Nora Aldossary on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ routineTick/routineTickApp.swift
@@ -0,0 +1,17 @@
+//
+//  routineTickApp.swift
+//  routineTick
+//
+//  Created by Nora Aldossary on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct routineTickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
