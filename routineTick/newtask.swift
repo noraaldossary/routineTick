900be5d1c48fd74a6e12c1aac979bd2a811e6679
Amal44b <amal4488vvvv@gmail.com>
2024-01-28 09:57:10 +0300

    Add model

--- routineTick/newtask.swift
+++ routineTick/newtask.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 import SwiftData
-//@Model
+@Model
+class NewTask {
+    var title: String
+    var descr: String
+    var todaytime : Data
+    var todaystart : Data
+    var todayend : Data
+    init(title: String = "", descr: String = "", todaytime: Data = Data(), todaystart: Data = Data(), todayend: Data = Data()) {
+        self.title = title
+        self.descr = descr
+        self.todaytime = todaytime
+        self.todaystart = todaystart
+        self.todayend = todayend
+    }
+    
+    
+}
 
