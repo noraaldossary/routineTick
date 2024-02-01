cddbd5ff53dc0cdfa1ff9e2a054202fb2f804a74
deemh <de.alboqmi@gmail.com>
2024-02-01 10:25:56 +0300

    edit

--- routineTick/RoleView.swift
+++ routineTick/RoleView.swift
@@ -4,13 +4,12 @@
 //
 //  Created by Deemh Albaqami on 11/07/1445 AH.
 //
-
-
 import SwiftUI
+import CloudKit
 
 struct RoleView: View {
-    
     @State private var selectedRole: String? = nil
+    let container = CKContainer(identifier: "iCloud.routineTick")
 
     var body: some View {
         ZStack {
@@ -41,8 +40,6 @@ struct RoleView: View {
                             .padding()
                             .background(selectedRole == "maids" ? Color("MainColor") : Color.gray.opacity(0.10))
                             .cornerRadius(40)
-                        
-                        
                     }
                     .padding()
                     
@@ -57,16 +54,17 @@ struct RoleView: View {
                     }
                 }
                 .padding()
-                
+                //هنا الاكشن
                 Button(action: {
-                    // Handle start action
                     if let selectedRole = selectedRole {
+                        saveRoleToCloudKit(role: selectedRole)
+                        
                         switch selectedRole {
                         case "maids":
-                            // Navigate to MaidsView or perform action for maids
+// يروح لفيو العامله ، بس تقرا بدون تعديل وحذف والخ
                             break
                         case "housewife":
-                            // Navigate to HousewifeView or perform action for housewife
+                            //يروح لفيو الام (كل الصلاحيات)
                             break
                         default:
                             break
@@ -93,6 +91,19 @@ struct RoleView: View {
             selectedRole = role
         }
     }
+// هنا تحفظ لي الرول في التيبل
+    private func saveRoleToCloudKit(role: String) {
+        let record = CKRecord(recordType: "UserRole")
+        record["role"] = role as CKRecordValue
+
+        container.privateCloudDatabase.save(record) { _, error in
+            if let error = error {
+                print("Error saving role to CloudKit: \(error.localizedDescription)")
+            } else {
+                print("done")
+            }
+        }
+    }
 }
 
 struct RoleView_Previews: PreviewProvider {
@@ -100,6 +111,102 @@ struct RoleView_Previews: PreviewProvider {
         RoleView()
     }
 }
+
+//
+//import SwiftUI
+//
+//struct RoleView: View {
+//    
+//    @State private var selectedRole: String? = nil
+//
+//    var body: some View {
+//        ZStack {
+//            Image("Imagebackground")
+//            
+//            VStack {
+//                Spacer()
+//                Image("RoleImage")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 350, height: 350)
+//                    
+//                Text("Select Your Role")
+//                    .font(.title)
+//                    .fontWeight(.bold)
+//                    .padding(.horizontal)
+//                
+//                Text("Specify your role in the household. Are you the housewife managing tasks or the maid executing them?")
+//                    .multilineTextAlignment(.center)
+//                    .padding(.horizontal)
+//                
+//                HStack {
+//                    Button(action: {
+//                        selectRole("maids")
+//                    }) {
+//                        Text("Maids")
+//                            .foregroundColor(selectedRole == "maids" ? .white : .black)
+//                            .padding()
+//                            .background(selectedRole == "maids" ? Color("MainColor") : Color.gray.opacity(0.10))
+//                            .cornerRadius(40)
+//                        
+//                        
+//                    }
+//                    .padding()
+//                    
+//                    Button(action: {
+//                        selectRole("housewife")
+//                    }) {
+//                        Text("Housewife")
+//                            .foregroundColor(selectedRole == "housewife" ? .white : .black)
+//                            .padding()
+//                            .background(selectedRole == "housewife" ? Color("MainColor") : Color.gray.opacity(0.10))
+//                            .cornerRadius(40)
+//                    }
+//                }
+//                .padding()
+//                
+//                Button(action: {
+//                    // Handle start action
+//                    if let selectedRole = selectedRole {
+//                        switch selectedRole {
+//                        case "maids":
+//                            // Navigate to MaidsView or perform action for maids
+//                            break
+//                        case "housewife":
+//                            // Navigate to HousewifeView or perform action for housewife
+//                            break
+//                        default:
+//                            break
+//                        }
+//                    }
+//                }) {
+//                    Text("Start")
+//                        .padding()
+//                        .foregroundColor(.white)
+//                        .background(selectedRole != nil ? Color("MainColor") : Color("MainColor").opacity(0.5))
+//                        .cornerRadius(40)
+//                }
+//                .disabled(selectedRole == nil)
+//                
+//                Spacer()
+//            }
+//        }
+//    }
+//
+//    private func selectRole(_ role: String) {
+//        if selectedRole == role {
+//            selectedRole = nil
+//        } else {
+//            selectedRole = role
+//        }
+//    }
+//}
+//
+//struct RoleView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        RoleView()
+//    }
+//}
 //
 //
 //import SwiftUI
